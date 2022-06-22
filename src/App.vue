697578b470d0fdc502083d41e57a7c03136f1e4f
Ahbali El-Mehdi <el.ahbali@gmail.com>
2022-06-22 14:29:28 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+import { reactive, ref } from 'vue';
+const counter = reactive({
+    count: 0
+})
+console.log(counter.count)
+counter.count++
+
+</script>
+
+<template>
+    <h1>Hello world</h1>
+</template>
+
+<style>
+</style>

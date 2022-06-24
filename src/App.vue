9f731e4671e3c1494844f9e740f5ece9157464d1
Ahbali El-Mehdi <el.ahbali@gmail.com>
2022-06-25 00:35:31 +0100

    part 8: computed Property

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 // import HelloWorld from './components/HelloWorld.vue'
-import { reactive, ref } from 'vue';
+import { computed, reactive, Ref, ref } from 'vue';
 const counter = reactive({
     count: 0
 })
@@ -35,58 +35,90 @@ function toggle() {
     awesome.value = !awesome.value
 }
 // part 7: List Rendering
+// part 8: computed Property
 type Todo = {
     id: number;
     text: string;
+    done: boolean
 }
 let id = 0
 const newTodo = ref('')
-const todos = ref([
-    { id: id++, text: 'Learn HTML' },
-    { id: id++, text: 'Learn JavaScript' },
-    { id: id++, text: 'Learn Vue' }
+const hideCompleted = ref(false)
+const todos: Ref<Array<Todo>> = ref([
+    { id: id++, text: 'Learn HTML', done: true },
+    { id: id++, text: 'Learn JavaScript', done: true },
+    { id: id++, text: 'Learn Vue', done: false }
 ])
 
 function addTodo(e: Event) {
-    todos.value = [{ id: id++, text: newTodo.value }, ...todos.value]
+    todos.value = [...todos.value, { id: id++, text: newTodo.value, done: false }]
     newTodo.value = ''
 }
 function removeTodo(todo: Todo) {
     todos.value = todos.value.filter(old => old.id !== todo.id)
 }
+const filteredTodos = computed(() => {
+    return hideCompleted.value ? todos.value.filter(todo => todo.done === false) : todos.value
+})
 </script>
 
 <template>
     <div class="col">
-        <h1>assignment: {{ text.message }}</h1>
-        <h1>{{ message.split('').reverse().join('') }}</h1>
-        <p>count is: {{ counter.count }}</p>
-        <h1 :class="titleClass">make me red</h1>
-        <button @click="increment" class="btn btn-primary">count is: {{ count }}</button>
-        <input :value="newText" @input="onInput" placeholder="try to type">
-        <input v-model="newText" placeholder="Type here">
-        <p>{{ newText }}</p>
+        <div class="row">
+            <div class="col-6">
+                <h1>assignment: {{ text.message }}</h1>
+                <h1>{{ message.split('').reverse().join('') }}</h1>
+                <p>count is: {{ counter.count }}</p>
+                <h1 :class="titleClass">make me red</h1>
+                <button @click="increment" class="btn btn-primary">count is: {{ count }}</button>
+                <input :value="newText" @input="onInput" placeholder="try to type">
+                <input v-model="newText" placeholder="Type here">
+                <p>{{ newText }}</p>
+            </div>
+        </div>
         <!-- part 6: conditional rendering -->
-        <button class="btn btn-danger" @click="toggle">toggle</button>
-        <h1 v-if="awesome">Vue is awesome!</h1>
-        <h1 v-else>Oh no :`(</h1>
+        <div class="row">
+            <div class="col-6">
+                <h1>part 6: conditional rendering</h1>
+                <button class="btn btn-danger" @click="toggle">toggle</button>
+                <h3 v-if="awesome">Vue is awesome!</h3>
+                <h3 v-else>Oh no :`(</h3>
+            </div>
+        </div>
 
         <!-- part 7: List Rendering -->
-        <form @submit.prevent="addTodo">
-            <input v-model="newTodo">
-            <button>Add Todo</button>
-        </form>
-        <ul>
-            <li v-for="todo in todos" :key="todo.id">
-                {{ todo.text }}
-                <button @click="removeTodo(todo)">X</button>
-            </li>
-        </ul>
+        <!-- part 8: computed Property -->
+        <div class="row">
+            <div class="col-6">
+                <h1>part 7: List Rendering</h1>
+                <h1>part 8: computed Property</h1>
+                <form @submit.prevent="addTodo">
+                    <input v-model="newTodo" placeholder="put your todo here">
+                    <button>Add Todo</button>
+                </form>
+                <ul>
+                    <li v-for="todo in filteredTodos" :key="todo.id">
+                        <input type="checkbox" v-model="todo.done">
+                        <span :class="{ done: todo.done }">
+                            {{ todo.text }}
+                        </span>
+                        <button @click="removeTodo(todo)">X</button>
+                    </li>
+                </ul>
+                <button @click="hideCompleted = !hideCompleted">
+                    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 
 <style>
 .title {
     color: red;
+}
+
+.done {
+    text-decoration: line-through;
 }
 </style>

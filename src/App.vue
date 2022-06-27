795a2e5e20e142cdda7943e1235146325409143c
Ahbali El-Mehdi <el.ahbali@gmail.com>
2022-06-27 16:14:08 +0100

    add prettier and eslint to the project

--- src/App.vue
+++ src/App.vue
@@ -1,180 +1,186 @@
 <script setup lang="ts">
-import { computed, onMounted, reactive, Ref, ref, watch } from 'vue';
-import ChildComp from './components/ChildComp.vue';
-const counter = reactive({
-    count: 0
-})
-console.log(counter.count)
-counter.count++
+  import { computed, onMounted, reactive, Ref, ref, watch } from 'vue'
+  import ChildComp from './components/ChildComp.vue'
+  const counter = reactive({
+    count: 0,
+  })
+  console.log(counter.count)
+  counter.count++
 
-const message = ref("hello world")
-console.log(message.value)
-message.value = 'changed'
+  const message = ref('hello world')
+  console.log(message.value)
+  message.value = 'changed'
 
-const text = reactive({
-    message: 'hellooooo'
-})
+  const text = reactive({
+    message: 'hellooooo',
+  })
 
-const titleClass = ref('title')
-const count = ref(0)
+  const titleClass = ref('title')
+  const count = ref(0)
 
-function increment() {
+  function increment() {
     count.value++
-}
+  }
 
-const newText = ref('')
-function onInput(e: Event) {
+  const newText = ref('')
+  function onInput(e: Event) {
     const elm = e.target as HTMLInputElement
     newText.value = elm.value
-}
-// part 6: conditional Rendering
-const awesome = ref(true)
-function toggle() {
+  }
+  // part 6: conditional Rendering
+  const awesome = ref(true)
+  function toggle() {
     awesome.value = !awesome.value
-}
-// part 7: List Rendering
-// part 8: computed Property
-type Todo = {
-    id: number;
-    text: string;
+  }
+  // part 7: List Rendering
+  // part 8: computed Property
+  type Todo = {
+    id: number
+    text: string
     done: boolean
-}
-let id = 0
-const newTodo = ref('')
-const hideCompleted = ref(false)
-const todos: Ref<Array<Todo>> = ref([
+  }
+  let id = 0
+  const newTodo = ref('')
+  const hideCompleted = ref(false)
+  const todos: Ref<Array<Todo>> = ref([
     { id: id++, text: 'Learn HTML', done: true },
     { id: id++, text: 'Learn JavaScript', done: true },
-    { id: id++, text: 'Learn Vue', done: false }
-])
+    { id: id++, text: 'Learn Vue', done: false },
+  ])
 
-function addTodo(e: Event) {
-    todos.value = [...todos.value, { id: id++, text: newTodo.value, done: false }]
+  function addTodo() {
+    todos.value = [
+      ...todos.value,
+      { id: id++, text: newTodo.value, done: false },
+    ]
     newTodo.value = ''
-}
-function removeTodo(todo: Todo) {
-    todos.value = todos.value.filter(old => old.id !== todo.id)
-}
-const filteredTodos = computed(() => {
-    return hideCompleted.value ? todos.value.filter(todo => todo.done === false) : todos.value
-})
-// part 9: Lifecycle and Template Refs
-const p = ref<HTMLParagraphElement>()
-onMounted(() => {
+  }
+  function removeTodo(todo: Todo) {
+    todos.value = todos.value.filter((old) => old.id !== todo.id)
+  }
+  const filteredTodos = computed(() => {
+    return hideCompleted.value
+      ? todos.value.filter((todo) => todo.done === false)
+      : todos.value
+  })
+  // part 9: Lifecycle and Template Refs
+  const p = ref<HTMLParagraphElement>()
+  onMounted(() => {
     if (p.value?.textContent) {
-        p.value.textContent = "onMounted value"
+      p.value.textContent = 'onMounted value'
     }
-})
-// part 10: Watchers
-const count10 = ref(0)
-watch(count10, (newCount) => {
+  })
+  // part 10: Watchers
+  const count10 = ref(0)
+  watch(count10, (newCount) => {
     console.log(`new count is : ${newCount}`)
-})
-const todoId = ref(1)
-const todoData = ref(null)
+  })
+  const todoId = ref(1)
+  const todoData = ref(null)
 
-async function fetchData() {
+  async function fetchData() {
     todoData.value = null
-    const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId.value}`)
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+    )
     todoData.value = await res.json()
-}
+  }
 
-fetchData()
-watch(todoId, fetchData)
-// part 12: Props
-const greeting = ref('Message from parent via Props')
-// part 13: Emits
-const childMsg = ref('No child msg yet')
-// part 14: Slots
-const parentMsg = ref('Message from the parent via Slots')
-
+  fetchData()
+  watch(todoId, fetchData)
+  // part 12: Props
+  const greeting = ref('Message from parent via Props')
+  // part 13: Emits
+  const childMsg = ref('No child msg yet')
+  // part 14: Slots
+  const parentMsg = ref('Message from the parent via Slots')
 </script>
 
 <template>
-    <div class="col">
-        <div class="row">
-            <div class="col-6">
-                <h1>assignment: {{ text.message }}</h1>
-                <h1>{{ message.split('').reverse().join('') }}</h1>
-                <p>count is: {{ counter.count }}</p>
-                <h1 :class="titleClass">make me red</h1>
-                <button @click="increment" class="btn btn-primary">count is: {{ count }}</button>
-                <input :value="newText" @input="onInput" placeholder="try to type">
-                <input v-model="newText" placeholder="Type here">
-                <p>{{ newText }}</p>
-            </div>
-        </div>
-        <!-- part 6: conditional rendering -->
-        <div class="row">
-            <div class="col-6">
-                <h1>part 6: conditional rendering</h1>
-                <button class="btn btn-danger" @click="toggle">toggle</button>
-                <h3 v-if="awesome">Vue is awesome!</h3>
-                <h3 v-else>Oh no :`(</h3>
-            </div>
-        </div>
-
-        <!-- part 7: List Rendering -->
-        <!-- part 8: computed Property -->
-        <div class="row">
-            <div class="col-6">
-                <h1>part 7: List Rendering</h1>
-                <h1>part 8: computed Property</h1>
-                <form @submit.prevent="addTodo">
-                    <input v-model="newTodo" placeholder="put your todo here">
-                    <button>Add Todo</button>
-                </form>
-                <ul>
-                    <li v-for="todo in filteredTodos" :key="todo.id">
-                        <input type="checkbox" v-model="todo.done">
-                        <span :class="{ done: todo.done }">
-                            {{ todo.text }}
-                        </span>
-                        <button @click="removeTodo(todo)">X</button>
-                    </li>
-                </ul>
-                <button @click="hideCompleted = !hideCompleted">
-                    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
-                </button>
-            </div>
-        </div>
-        <!-- part 9: Lifecycle and Template Refs -->
-        <div class="row">
-            <p ref="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus temporibus pariatur
-                praesentium cumque eligendi culpa. Minus, laudantium distinctio? Perspiciatis aliquid distinctio
-                voluptatibus id, aperiam soluta laboriosam? Molestias, cupiditate qui! Voluptatibus.</p>
-        </div>
-        <!-- part 10: Watchers -->
-        <div class="row">
-            <div class="col-6">
-                <button @click="count10++">increment</button>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-10">
-                <p>Todo id: {{ todoId }}</p>
-                <button @click="todoId++">Fetch next todo</button>
-                <p v-if="!todoData">Loading...</p>
-                <pre v-else>{{ todoData }}</pre>
-            </div>
-        </div>
+  <div class="col">
+    <div class="row">
+      <div class="col-6">
+        <h1>assignment: {{ text.message }}</h1>
+        <h1>{{ message.split('').reverse().join('') }}</h1>
+        <p>count is: {{ counter.count }}</p>
+        <h1 :class="titleClass">make me red</h1>
+        <button class="btn btn-primary" @click="increment">
+          count is: {{ count }}
+        </button>
+        <input :value="newText" placeholder="try to type" @input="onInput" />
+        <input v-model="newText" placeholder="Type here" />
+        <p>{{ newText }}</p>
+      </div>
     </div>
-    <!-- part 11: Components -->
-    <!-- part 12: Props -->
-    <!-- part 13: Emits -->
-    <ChildComp :msg="greeting" @response="(msg) => childMsg = msg">
-        <!-- part 14: Slots -->
-        <h3>{{ parentMsg }}</h3>
-    </ChildComp>
-    <p>{{ childMsg }}</p>
+    <!-- part 6: conditional rendering -->
+    <div class="row">
+      <div class="col-6">
+        <h1>part 6: conditional rendering</h1>
+        <button class="btn btn-danger" @click="toggle">toggle</button>
+        <h3 v-if="awesome">Vue is awesome!</h3>
+        <h3 v-else>Oh no :`(</h3>
+      </div>
+    </div>
+
+    <!-- part 7: List Rendering -->
+    <!-- part 8: computed Property -->
+    <div class="row">
+      <div class="col-6">
+        <h1>part 7: List Rendering</h1>
+        <h1>part 8: computed Property</h1>
+        <form @submit.prevent="addTodo">
+          <input v-model="newTodo" placeholder="put your todo here" />
+          <button>Add Todo</button>
+        </form>
+        <ul>
+          <li v-for="todo in filteredTodos" :key="todo.id">
+            <input v-model="todo.done" type="checkbox" />
+            <span :class="{ done: todo.done }">
+              {{ todo.text }}
+            </span>
+            <button @click="removeTodo(todo)">X</button>
+          </li>
+        </ul>
+        <button @click="hideCompleted = !hideCompleted">
+          {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+        </button>
+      </div>
+    </div>
+    <!-- part 9: Lifecycle and Template Refs -->
+    <div class="row">
+      <p ref="p">initial value</p>
+    </div>
+    <!-- part 10: Watchers -->
+    <div class="row">
+      <div class="col-6">
+        <button @click="count10++">increment</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-10">
+        <p>Todo id: {{ todoId }}</p>
+        <button @click="todoId++">Fetch next todo</button>
+        <p v-if="!todoData">Loading...</p>
+        <pre v-else>{{ todoData }}</pre>
+      </div>
+    </div>
+  </div>
+  <!-- part 11: Components -->
+  <!-- part 12: Props -->
+  <!-- part 13: Emits -->
+  <ChildComp :msg="greeting" @response="(msg) => (childMsg = msg)">
+    <!-- part 14: Slots -->
+    <h3>{{ parentMsg }}</h3>
+  </ChildComp>
+  <p>{{ childMsg }}</p>
 </template>
 
 <style>
-.title {
+  .title {
     color: red;
-}
+  }
 
-.done {
+  .done {
     text-decoration: line-through;
-}
+  }
 </style>
--- src/components/ChildComp.vue
+++ src/components/ChildComp.vue
@@ -1,23 +1,21 @@
 <script setup lang="ts">
-// part 12: Props
-const props = defineProps<{
-    msg?: String
-}>()
-// part 13: Emits
-const emit = defineEmits<{
+  // part 12: Props
+  const props = defineProps<{
+    msg?: string
+  }>()
+  // part 13: Emits
+  const emit = defineEmits<{
     (event: 'response', msg: string): void
-}>()
-emit('response', 'hello from child')
-
+  }>()
+  emit('response', 'hello from child')
 </script>
 
 <template>
-    <div class="row">
-        <h2>{{ msg ?? 'No props passed yet' }}</h2>
-    </div>
-    <!-- part 14: Slots -->
-    <slot>Fallback content</slot>
+  <div class="row">
+    <h2>{{ msg ?? 'No props passed yet' }}</h2>
+  </div>
+  <!-- part 14: Slots -->
+  <slot>Fallback content</slot>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
